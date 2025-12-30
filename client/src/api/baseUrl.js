d6d7f034e139205a22c7686cd675d0cc23c57cf4
himanisaha <himanisaha3@gmail.com>
2025-12-30 17:53:44 +0530

    fix: production BASE_URL

--- client/src/api/baseUrl.js
+++ client/src/api/baseUrl.js
@@ -1,6 +1,6 @@
 // export const BASE_URL = "http://localhost:8000";
 // src/api/baseUrl.js
-const isDev = import.meta.env.DEV;
+const isDev = import.meta.env.DEV && window.location.hostname === 'localhost';
 export const BASE_URL = isDev 
   ? "http://localhost:8000" 
   : "https://e-commerce-website-amazon-clone-production.up.railway.app";
